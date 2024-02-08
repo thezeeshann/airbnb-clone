4ee6203a52ff6cd554b3142b4408827ead9c10d6
thezeeshann <info.zeeshann@gmail.com>
2024-02-08 16:34:08 +0530

    feat: Added social login(Google & Github)

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -59,13 +60,13 @@ const RegisterModal = () => {
         outline 
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={()=>{}} 
+        onClick={()=>signIn('google')} 
       />
       <Button 
         outline 
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={()=>{}}
+        onClick={()=>signIn('github')}
       />
       <div 
         className="
